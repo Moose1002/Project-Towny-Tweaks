fb7b2500b64ccafcd6868c3676a7403fb7ab751a
Moose1002 <33578674+Moose1002@users.noreply.github.com>
2020-11-11 11:13:09 -0500

    Update Reference.java

--- src/main/java/moose/projecttownytweaks/common/util/Reference.java
+++ src/main/java/moose/projecttownytweaks/common/util/Reference.java
@@ -6,6 +6,4 @@ public class Reference {
 	public static final String NAME = "Project Towny Tweaks";
 	public static final String VERSION = "0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "moose.projecttownytweaks.common.proxy.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "moose.projecttownytweaks.common.proxy.CommonProxy";
 }
